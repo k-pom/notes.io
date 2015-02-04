f1682364b1408b218e5232992b7b20ef47d94409
Kaleb Pomeroy <kaleb.pomeroy@rackspace.com>
2015-02-04 14:35:02 -0600

    Pagination and truncation fixes

--- notes/__init__.py
+++ notes/__init__.py
@@ -14,9 +14,15 @@ def datetimeformat(value, format="%Y-%m-%dT%H:%M:%SZ", out="%B %d, %Y"):
 
 
 @app.template_filter('get_as_markdown')
-def get_as_markdown(raw_url):
+def get_as_markdown(raw_url, limit=False):
+    """
+        Given the URL, fetch it. If a limit is set, truncate it after the
+    """
 
     markdown = requests.get(raw_url).text
+
+    if limit:
+        markdown = markdown[0:(limit+markdown[limit:].index('\n\n')+1)]
 
     github_user = os.environ.get('GITHUB_USER')
     github_pass = os.environ.get('GITHUB_PASS')
--- notes/home.py
+++ notes/home.py
@@ -3,24 +3,37 @@ from flask import render_template
 from notes import app
 import os
 
+def get_gists_for_user(username, page=0):
 
-@app.route("/")
-def home():
-
-    return render_template("home.html", username="")
-
-
-@app.route("/<username>")
-def list(username):
-
+    items_per_page = 2
     github_user = os.environ.get('GITHUB_USER')
     github_pass = os.environ.get('GITHUB_PASS')
     url = "https://api.github.com/users/%s/gists" % username
 
     gists = requests.get(url, auth=(github_user, github_pass)).json()
+    gists = [g for g in gists if g['description'] != "description" and g['files'][g['files'].keys()[0]]['language'] == "Markdown"]
 
-    return render_template("list.html", username=username, gists=gists)
+    start = page*items_per_page
 
+    if start+items_per_page > len(gists):
+        gists.append(None)
+
+    return gists[start:start+items_per_page]
+
+@app.route("/")
+def home():
+    return render_template("home.html", username="")
+
+
+@app.route("/<username>/page/<page>")
+@app.route("/<username>")
+def list(username, page=0):
+    page = int(page)
+    gists = get_gists_for_user(username, int(page))
+    return render_template("list.html",
+                           page=page,
+                           username=username,
+                           gists=gists)
 
 @app.route("/<username>/<id>")
 def show(username, id):
